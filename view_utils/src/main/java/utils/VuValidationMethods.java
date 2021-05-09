f28d9fa7311260bbf2613fb5f2c24d7b3c4d4b8e
Samarth Thesiya <samarththesiya@Samarths-mac.local>
2021-05-09 13:12:07 +0530

    Added support of Regex

--- view_utils/src/main/java/utils/VuValidationMethods.java
+++ view_utils/src/main/java/utils/VuValidationMethods.java
@@ -2,6 +2,8 @@ package utils;
 
 import android.text.TextUtils;
 
+import java.util.regex.Pattern;
+
 import exceptions.InvalidArgumentException;
 import interfaces.VuValidatable;
 import models.Validations;
@@ -9,8 +11,10 @@ import models.Validations;
 public class VuValidationMethods {
 
     protected VuValidatable vuValidatable;
+
     public static final String REQUIRED   = "required";
     public static final String MIN_LENGTH = "minLength";
+    public static final String REGEX      = "regex";
 
     public void setVuValidatable(VuValidatable vuValidatable) {
         this.vuValidatable = vuValidatable;
@@ -42,6 +46,25 @@ public class VuValidationMethods {
         if (vuValidatable.getText().length() < minLength) {
             if (TextUtils.isEmpty(validation.getMessage())) {
                 validation.setMessage("Too few characters");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean regex(Validations.Validation validation) {
+
+        if (validation.getExtra() == null) {
+            throw new InvalidArgumentException("Regex needs to pass in 2nd argument of models.Validations.add()");
+        } else if (!(validation.getExtra() instanceof String)) {
+            throw new InvalidArgumentException("Regex should be string");
+        }
+
+        String regex = (String) validation.getExtra();
+
+        if (!Pattern.matches(regex, vuValidatable.getText())) {
+            if (TextUtils.isEmpty(validation.getMessage())) {
+                validation.setMessage("Invalid input");
             }
             return false;
         }
